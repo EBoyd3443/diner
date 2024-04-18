9aea8d09aa2ea23ee68c7b3b66aa5058e9414bba
Eric Boyd <boyd.eric@student.greenriver.edu>
2024-04-18 12:33:36 -0400

    Add menus and order forms

--- index.php
+++ index.php
@@ -51,5 +51,47 @@ $f3->route('GET /menus/dinner', function() {
     echo $view->render("views/dinner-menu.html");
 });
 
+// Define the order1 route
+$f3->route('GET|POST /order1', function($f3) {
+    //echo '<h1>Hello Diners. Welcome to My Diner App!</h1>';
+
+    //If the method has been posted
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        //if data valid
+        if(!empty($food) && !empty($meal))//very simple validation for example
+        {
+            //add data to session array
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
+            //send the user to the next form
+            $f3->reroute('order2');
+        }
+        else
+        {
+            echo "<p>Validation failed</p>"; //Temporary! Don't mix code types.
+        }
+    }
+
+
+    //Render view page.
+    $view = new Template();
+    echo $view->render("views/order1.html");
+});
+
+// Define the order2 route
+$f3->route('GET|POST /order2', function($f3) {
+    //echo '<h1>Hello Diners. Welcome to My Diner App!</h1>';
+
+    var_dump( $f3->get('SESSION'));
+
+    //Render view page.
+    $view = new Template();
+    echo $view->render("views/order2.html");
+});
+
 // Run fat free
 $f3->run();
